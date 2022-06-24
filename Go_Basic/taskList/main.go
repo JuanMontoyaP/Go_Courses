8adf11d533d27382d4567ea86cb319dca077be83
Juan Pablo M <jpmontoya19@gmail.com>
2022-06-23 23:10:37 -0500

    Class 15: Slices

--- Go_Basic/taskList/main.go
+++ Go_Basic/taskList/main.go
@@ -20,18 +20,51 @@ func (t *task) markCompletedTask() {
 	t.completed = true
 }
 
+type taskList struct {
+	tasks []*task
+}
+
+func (tl *taskList) addTaskToList(t *task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *taskList) removeTaskFromList(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
 func main() {
-	t := task{
+	t1 := &task{
 		name:        "Complete my go course",
 		description: "Complete my go course this week",
 		completed:   false,
 	}
 
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		name:        "Complete my Python course",
+		description: "Complete my Python course this week",
+		completed:   false,
+	}
 
-	t.markCompletedTask()
-	t.updateName("Finish my go course")
-	t.updateDescription("Finish my go course asap")
+	t3 := &task{
+		name:        "Complete my NodeJS course",
+		description: "Complete my NodeJS course this week",
+		completed:   false,
+	}
 
-	fmt.Printf("%+v\n", t)
+	list := taskList{
+		tasks: []*task{
+			t1,
+			t2,
+		},
+	}
+
+	fmt.Println(list.tasks[0])
+
+	list.addTaskToList(t3)
+
+	fmt.Println(len(list.tasks))
+
+	list.removeTaskFromList(1)
+	fmt.Println(len(list.tasks))
+
 }
